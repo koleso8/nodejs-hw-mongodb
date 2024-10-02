fa779406c31539c43eb2bf4c3a9f554fab8e3472
koleso8 <dev.koleso@gmail.com>
2024-10-02 13:34:41 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+const PORT = 3000;
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        targe: 'pino-pretty',
+      },
+    })
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
